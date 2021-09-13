6655b194a6f4fbe1e9122a4e51961b28e623103d
coldpigli <piyushdas98@gmail.com>
2021-09-13 22:48:09 +0530

    fetched data via Axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 function App() {
   
+  const [searchTerm, setSearchTerm] = useState('whiplash');
+  const [meanings, setMeanings] = useState([]);
+
+  useEffect(()=>{
+    //calling API here
+    const searchCall = async () =>{
+      const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: searchTerm,
+        },
+      });
+      setMeanings(data.query.search);
+    }
+    searchCall();
+
+  },[searchTerm]);
   
   return (
-  <h2>lorem ipsum project setup</h2>
+    <div className = "ui container">
+      <h1>Wiki-Search</h1>
+      <div className = "ui form">
+        <div className = "field">
+          <input
+            className = "input"
+            placeholder = "Look up meanings"
+            value = {searchTerm}
+            onChange = {(e)=>{setSearchTerm(e.target.value)}}
+          />
+        </div>
+
+      </div>
+    </div>
     )
 }
 
